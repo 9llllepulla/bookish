d1244db91c10e1998024825a9e2a2dea81788203
sergeylyashko <slyashko@mail.ru>
2022-02-12 19:49:36 +0300

    added logging to project

--- api/src/main/java/com/Application.java
+++ api/src/main/java/com/Application.java
@@ -1,20 +1,21 @@
 package com;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.builder.SpringApplicationBuilder;
-import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 
+import java.util.Map;
+
+@Slf4j
 @SpringBootApplication
-public class Application extends SpringBootServletInitializer {
+@EntityScan (basePackages = "com")
+public class Application {
 
     public static void main(String[] args) {
-        SpringApplication.run(Application.class, args);
+        log.info("application started");
+        SpringApplication application = new SpringApplication(Application.class);
+        application.setDefaultProperties(Map.of("spring.config.name", "app"));
+        application.run(args);
     }
-
-    @Override
-    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
-        return application.sources(Application.class);
-    }
-
 }
