9b0c0519acfeb43ee92ea6deb8526a687c99eae0
sergeylyashko <slyashko@mail.ru>
2022-02-13 16:34:34 +0300

    refactoring packages

--- core/src/main/java/com/gllllepulla/configuration/ApiConfig.java
+++ core/src/main/java/com/gllllepulla/configuration/ApiConfig.java
@@ -3,8 +3,8 @@ package com.gllllepulla.configuration;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
-@ComponentScan(basePackages = {"com.controller"})
+@ComponentScan (basePackages = {"com.gllllepulla.controller"})
 @Configuration
-public class ApiConfiguration {
+public class ApiConfig {
 
 }
