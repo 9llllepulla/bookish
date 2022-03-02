3ad6555a196955b6a2da6dfe5300b1a8d235c527
sergeylyashko <slyashko@mail.ru>
2022-03-02 22:54:54 +0300

    added annotations

--- storage/src/main/java/com/gllllepulla/configuration/StorageConfiguration.java
+++ storage/src/main/java/com/gllllepulla/configuration/StorageConfiguration.java
@@ -3,9 +3,11 @@ package com.gllllepulla.configuration;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 
+@Configuration
 @EntityScan(basePackages = {"com.gllllepulla.entities"})
 @EnableJpaRepositories(basePackages = {"com.gllllepulla.repository"})
-@Configuration
+@EnableTransactionManagement
 public class StorageConfiguration {
 }
