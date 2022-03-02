03815a72a61fb16ec243c806aa47a600866e80e0
sergeylyashko <slyashko@mail.ru>
2022-03-02 22:53:35 +0300

    added server properties

--- core/src/main/java/com/gllllepulla/Application.java
+++ core/src/main/java/com/gllllepulla/Application.java
@@ -3,17 +3,18 @@ package com.gllllepulla;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
 import java.util.Map;
 
 @Slf4j
-@SpringBootApplication
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
 public class Application {
 
     public static void main(String[] args) {
         log.info("application started");
         SpringApplication application = new SpringApplication(Application.class);
-        application.setDefaultProperties(Map.of("spring.config.name", "app"));
+        application.setDefaultProperties(Map.of("spring.config.name", "server"));
         application.run(args);
     }
 }
