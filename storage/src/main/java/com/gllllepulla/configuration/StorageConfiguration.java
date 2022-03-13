972d52140b139da82ee709a8ff1df961bda0f517
sergeylyashko <slyashko@mail.ru>
2022-03-13 22:41:25 +0300

    fixed error mapping of author name fields in json

--- storage/src/main/java/com/gllllepulla/configuration/StorageConfiguration.java
+++ storage/src/main/java/com/gllllepulla/configuration/StorageConfiguration.java
@@ -2,11 +2,13 @@ package com.gllllepulla.configuration;
 
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 @Slf4j
 @EntityScan(basePackages = "com.gllllepulla")
+@ComponentScan(basePackages = "com.gllllepulla.repository")
 public class StorageConfiguration {
 
 }
--- transfer/src/main/java/com/gllllepulla/mapper/BookMapperDecorator.java
+++ transfer/src/main/java/com/gllllepulla/mapper/BookMapperDecorator.java
@@ -1,8 +1,11 @@
 package com.gllllepulla.mapper;
 
 
+import com.gllllepulla.entities.Author;
 import com.gllllepulla.entities.Book;
+import com.gllllepulla.model.Dto;
 import com.gllllepulla.repository.AuthorRepository;
+import com.gllllepulla.repository.BookAuthorRepository;
 import com.gllllepulla.repository.GenreRepository;
 import com.gllllepulla.repository.PublisherRepository;
 import com.gllllepulla.model.Info;
@@ -22,6 +25,8 @@ public abstract class BookMapperDecorator implements BookMapper {
     private PublisherRepository publisherRepository;
     @Autowired
     private AuthorRepository authorRepository;
+    @Autowired
+    private BookAuthorRepository bookAuthorRepository;
 
 
     @Override
@@ -37,5 +42,16 @@ public abstract class BookMapperDecorator implements BookMapper {
                 .findFirst()
                 .ifPresent(book::addAuthor);
         return book;
+    }
+
+    @Override
+    public Dto.Book toBookDto(Book book) {
+        Dto.Book bookDto = delegate.toBookDto(book);
+        String authorName = bookAuthorRepository.findAuthorsByBookId(book.getId()).stream()
+                .findFirst()
+                .map(Author::getAuthorName)
+                .orElse(null);
+
+        return new Dto.Book(bookDto.id(), bookDto.title(), authorName, bookDto.yearPublication(), bookDto.genreName(), bookDto.publisherName());
     }
 }
