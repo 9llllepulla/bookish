50ca6fe18c4921bb649c93fbc3558f266b2627f8
sergeylyashko <slyashko@mail.ru>
2022-04-12 23:21:32 +0300

    changed controller

--- api/src/main/java/com/gllllepulla/controller/EditorController.java
+++ api/src/main/java/com/gllllepulla/controller/EditorController.java
@@ -1,58 +1,59 @@
 package com.gllllepulla.controller;
 
-
 import com.gllllepulla.service.EditorService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import com.gllllepulla.model.Info;
 
+import java.util.Map;
 import java.util.Set;
 
 @Slf4j
 @RestController
 @RequiredArgsConstructor
-@RequestMapping ("/editor")
+@RequestMapping("/editor")
 public class EditorController {
 
-    private final EditorService editorService;
+    @Autowired
+    private final Map<String, EditorService> services;
 
-    @PutMapping(value = "/authors")
+    @PutMapping(value = "authors")
     public ResponseEntity<Info.Author> editAuthor(@RequestParam Info.Author author) {
-        return editorService.editAuthor(author)
+        EditorService<Info.Author> editorService = services.get(ItemType.authors.name());
+        return editorService.edit(author)
                 .map(ResponseEntity::ok)
                 .orElse(ResponseEntity.notFound().build());
     }
 
-    @PostMapping("/authors")
+    @PostMapping("authors")
     public ResponseEntity<Info.Author> createAuthor(@RequestBody Info.Author author) {
+        EditorService<Info.Author> editorService = services.get(ItemType.authors.name());
         return ResponseEntity.ok()
-                .body(editorService.createAuthor(author));
-    }
-
-    @DeleteMapping("/authors")
-    public ResponseEntity<Void> deleteAuthorsByIds(@RequestParam Set<Long> ids) {
-        editorService.deleteAuthorsByIds(ids);
-        return ResponseEntity.ok().build();
+                .body(editorService.create(author));
     }
 
     @PutMapping(value = "/books")
     public ResponseEntity<Info.Book> editBook(@RequestParam Info.Book book) {
-        return editorService.editBook(book)
+        EditorService<Info.Book> editorService = services.get(ItemType.books.name());
+        return editorService.edit(book)
                 .map(ResponseEntity::ok)
                 .orElse(ResponseEntity.notFound().build());
     }
 
     @PostMapping("/books")
     public ResponseEntity<Info.Book> createBook(@RequestBody Info.Book book) {
+        EditorService<Info.Book> editorService = services.get(ItemType.books.name());
         return ResponseEntity.ok()
-                .body(editorService.createBook(book));
+                .body(editorService.create(book));
     }
 
-    @DeleteMapping("/books")
-    public ResponseEntity<Void> deleteBooksByIds(@RequestParam Set<Long> ids) {
-        editorService.deleteBooksByIds(ids);
+    @DeleteMapping("{type}")
+    public ResponseEntity<Void> deleteByIds(@PathVariable("type") ItemType type,
+                                            @RequestParam Set<Long> ids) {
+        services.get(type.name()).deleteByIds(ids);
         return ResponseEntity.ok().build();
     }
 
