0a99ef4cf963fdb648a7accec2d62dc0efac57a9
sergeylyashko <slyashko@mail.ru>
2022-05-22 21:53:21 +0300

    added login endpoint to authentical controller

--- api/src/main/java/com/gllllepulla/controller/AuthenticationController.java
+++ api/src/main/java/com/gllllepulla/controller/AuthenticationController.java
@@ -1,8 +1,10 @@
 package com.gllllepulla.controller;
 
+import com.gllllepulla.service.SecurityService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.*;
 
 import javax.servlet.http.HttpServletRequest;
@@ -14,19 +16,19 @@ import java.io.IOException;
 @RequestMapping("/auth")
 public class AuthenticationController {
 
+    private final SecurityService securityService;
 
     @PostMapping(value = "/login")
     public ResponseEntity<?> login(@RequestParam(value = "username") String userName,
                                    @RequestParam(value = "password") String password,
                                    HttpServletRequest request) throws IOException {
-
-        return ResponseEntity.notFound().build(); // TODO stub
-    }
-
-    @GetMapping(value = "/logout")
-    public ResponseEntity<?> logOut() {
-
-        return ResponseEntity.notFound().build(); // TODO stub
+        Authentication authenticate = securityService.authenticate(userName, password, request);
+        record LoginResponse(String response){}
+        if (authenticate.isAuthenticated()) {
+            return ResponseEntity.ok(new LoginResponse("authenticated"));
+        } else {
+            return ResponseEntity.status(401).body(new LoginResponse("not authenticated"));
+        }
     }
 
 }
